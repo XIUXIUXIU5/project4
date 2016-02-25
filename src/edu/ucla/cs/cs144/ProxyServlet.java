205e62e5c46ed8de5079d8cf25f1a65a2c9b3596
XIUXIUXIU5 <shaolei@ucla.edu>
2016-02-25 09:55:24 -0800

    check point

--- src/edu/ucla/cs/cs144/ProxyServlet.java
+++ src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -7,13 +7,52 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URL;
+import java.io.BufferedReader;
+import javax.servlet.ServletOutputStream;
+import java.io.InputStreamReader;
 
 public class ProxyServlet extends HttpServlet implements Servlet {
-       
+      private final String USER_AGENT = "Mozilla/5.0";
+    
     public ProxyServlet() {}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+    	String query = request.getParameter("q");
+
+        String url = "http://google.com/complete/search?output=toolbar&q="+query;
+         
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+        // optional default is GET
+        con.setRequestMethod("GET");
+ 
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+ 
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response1 = new StringBuffer();
+      
+      	response.addHeader("Access-Control-Allow-Origin", "*"); 	
+       	response.setContentType("text/xml");
+
+        ServletOutputStream sout = response.getOutputStream();
+  
+        while ((inputLine = in.readLine()) != null) {
+            response1.append(inputLine);
+            sout.write(inputLine.getBytes());
+        }
+        in.close();
+ 
+        sout.flush();
+ 
     }
 }
